15be5fbd08a6d21f306b7b73677f86ebc64b2873
BernardoModerno <bernardo.moderno@gmail.com>
2020-09-27 14:51:04 -0300

    ENTENDENDO OS CONTROLLERS

---
+++ App/controllers/contato.php
@@ -0,0 +1,19 @@
+<?php
+
+class Contato
+{
+    public function index()
+    {
+        echo "index do contato";
+    }
+
+    public function email()
+    {
+        echo "bernardo@gmail.com";
+    }
+
+    public function telefone()
+    {
+        echo "37 98989-9999";
+    }
+}
