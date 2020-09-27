35a7e5f7a69f1b3fe69e2904a819d88c4614fde4
BernardoModerno <bernardo.moderno@gmail.com>
2020-09-27 17:34:12 -0300

    ENTENDENDO OS MODELS

--- App/controllers/home.php
+++ App/controllers/home.php
@@ -1,9 +1,15 @@
 <?php
 
-class Home
+use \App\Core\Controller;
+
+class Home extends Controller
 {
-    public function index()
+    public function index($nome = '', $email = '')
     {
-        echo "estou na index";
+        $user = $this->model('User');
+        $user->nome = $nome;
+        $user->email = $email;
+
+        echo $user->nome."<br>".$user->email;
     }
 }
